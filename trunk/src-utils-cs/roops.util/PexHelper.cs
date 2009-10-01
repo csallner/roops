d23dfe376c12080ca9fbf091b94d511fd7f35e5c
Mainul <Mainul@0ebaf876-8e72-11de-a7db-6fd96749a44a>
2009-10-01 21:17:59 +0000

    c# utility files added which will help to generate latex comparison table

---
+++ trunk/src-utils-cs/roops.util/PexHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace roops.core.bv32.linear.noex.gods
+{
+    class PexHelper
+    {
+        public static void WriteToFile(string textToWrite)
+        {
+            string outputFileFullPath = "c:\\PexOutput.txt";
+            System.IO.StreamWriter log_out;
+
+            try
+            {
+                log_out = new System.IO.StreamWriter(outputFileFullPath, true);
+            }
+            catch (System.IO.IOException exc)
+            {
+                System.Console.WriteLine("Error: " + exc.Message + "Cannot open file.");
+                return;
+            }
+
+            // Direct standard output to the log file. 
+            System.Console.SetOut(log_out);
+            System.Console.WriteLine(textToWrite);
+            log_out.Close();
+        }
+    }
+}
