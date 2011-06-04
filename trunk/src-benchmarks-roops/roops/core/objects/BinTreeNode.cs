0dec9096f0e586bfe18b48dfb82bc12093614aff
jgaleotti@dc.uba.ar <jgaleotti@dc.uba.ar@0ebaf876-8e72-11de-a7db-6fd96749a44a>
2011-06-04 01:46:26 +0000

    Binary tree benchmark

---
+++ trunk/src-benchmarks-roops/roops/core/objects/BinTreeNode.cs
@@ -0,0 +1,18 @@
+//$category roops.core.objects
+
+//Authors: Marcelo Frias
+class BinTreeNode {
+
+	public int key;
+
+	public /*@ nullable @*/BinTreeNode left;
+
+	public /*@ nullable @*/BinTreeNode right;
+
+	public /*@ nullable @*/BinTreeNode parent;
+
+	public BinTreeNode() {}
+}
+
+//$endcategory roops.core.objects
+
