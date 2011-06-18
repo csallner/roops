a57cfc0765833fd95672c3fc974d9e7630a5e911
jgaleotti <jgaleotti@dc.uba.ar>
2011-06-18 22:26:16 +0000

    index out of bound test case

---
+++ src-benchmarks-roops/roops/extended/bv32/arr/BinSearchError.cs
@@ -0,0 +1,42 @@
+//$category roops.extended.bv32.arr
+//$benchmarkclass
+
+public class BinSearchError {
+
+       
+	//$goals 1
+	//$benchmark
+       public void test_bin_search(int[] a, int key) {
+         if (a!=null) {
+           int ret_val = binarySearch(a,key);
+         }
+       }	
+
+       public static int binarySearch(int[] a, int key) {
+		int low = 0;
+		int high = a.length - 1;
+		int mid;
+		int midVal ;
+		
+		while (low <= high)  {
+			mid = (low + high) /2;
+
+                        if (mid<0) {
+                          /*$goal 0 reachable*/
+                        }
+
+			midVal = a[mid];
+
+			if (midVal < key)
+				low = mid + 1;
+			else if (midVal > key)
+				high = mid - 1;
+			else {
+				return mid; // key found
+			}
+		}
+		
+		return -(low+1);
+	}
+}
+//$endcategory roops.extended.bv32.arr
